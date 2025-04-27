a92ba7fb2aa3ec20d692d87b5e8798ae594e81fc
Hisham C <hishamc18@gmail.com>
2025-04-28 05:06:16 +0530

    Readme Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import React from 'react'
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-const page = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/login");
+  }, [router]);
+
+  return null;
 }
-
-export default page
